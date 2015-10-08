d7be4e4f7b54f66fcea38cb0fe79a28042d41a70
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-10-08 09:28:42 +0000

    Add script to check for style issues
    git-svn-id: https://svnserver.informatik.kit.edu/i57/svn/Reve@970 c15e727d-c47f-405b-9667-70cf6562d04f

---
+++ cpp-example/check.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd build
+make clean
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+make
+./example ~/code/reve/Horn/experiments/production/while/loop_1.spl ~/code/reve/Horn/experiments/production/while/loop_2.spl
+sources=(../*.cpp)
+echo "Running clang-tidy"
+clang-tidy "${sources[@]}"
+echo "Running clang-modernize"
+clang-modernize -summary -p . -include ..
+echo "Running cncc"
+for file in ${sources[@]}
+do
+    cncc --style=../../.cncc.style --dbdir . "$file"
+done
