8fd29799492d12da8387928ee65c73a706b64e8e
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-10-25 17:26:47 +0000

    Move marks inside the loop
    git-svn-id: https://svnserver.informatik.kit.edu/i57/svn/Reve@1009 c15e727d-c47f-405b-9667-70cf6562d04f

--- examples/break_1.c
+++ examples/break_1.c
@@ -1,7 +1,8 @@
 extern int __mark(int);
 int f(int x) {
     int i = 0;
-    while (__mark(42) & (i <= 10)) {
+    while (/*__mark(42) &*/ (i <= 10)) {
+        __mark(42);
         if (i == x) {
             break;
         }
--- examples/break_2.c
+++ examples/break_2.c
@@ -1,7 +1,8 @@
 extern int __mark(int);
 int f(int x) {
     int i = 10;
-    while (__mark(42) & (i >= 0)) {
+    while (/*__mark(42) &*/ (i >= 0)) {
+        __mark(42);
         if (i == (10 - x)) {
             break;
         }
