4dc8d5bafc6a843ea759852f2cab648012765400
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-10-29 05:53:24 +0000

    Change path to examples
    git-svn-id: https://svnserver.informatik.kit.edu/i57/svn/Reve@1027 c15e727d-c47f-405b-9667-70cf6562d04f

--- reve/check.sh
+++ reve/check.sh
@@ -3,7 +3,7 @@
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make
-./reve ~/code/reve/Horn/experiments/production/while/loop_1.spl ~/code/reve/Horn/experiments/production/while/loop_2.spl
+./reve ../../examples/break_1.c ../../examples/break_2.c
 cd ..
 echo "Running clang-tidy"
 clang-tidy -p build *.cpp
