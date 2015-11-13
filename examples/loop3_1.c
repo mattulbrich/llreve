a00a55d1a147351e9d1f5bcdad2c53230d2674bf
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-11-13 12:21:40 +0000

    Add loop3 example
    git-svn-id: https://svnserver.informatik.kit.edu/i57/svn/Reve@1073 c15e727d-c47f-405b-9667-70cf6562d04f

---
+++ examples/loop3_1.c
@@ -0,0 +1,18 @@
+extern int __mark(int);
+
+int f(int n) {
+    int i = 1;
+    int j = 0;
+
+    if (n < 1) {
+        n = 1;
+    }
+
+    __mark(23);
+    while (__mark(42) & i <= n) {
+        j = j + 2;
+        i++;
+    }
+
+    return j;
+}
