da718d413f4a8f592d7253345e2770e1be1b75b6
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-12-01 17:29:08 +0000

    Add isSetTo example stolen from torch
    git-svn-id: https://svnserver.informatik.kit.edu/i57/svn/Reve@1118 c15e727d-c47f-405b-9667-70cf6562d04f

---
+++ examples/torch/issetto_2.c
@@ -0,0 +1,54 @@
+extern int __mark(int);
+// we don’t support floating points so use an int here
+#define real int
+#define TH_CONCAT_4_EXPAND(x,y,z,w) x ## y ## z ## w
+#define TH_CONCAT_4(x,y,z,w) TH_CONCAT_4_EXPAND(x,y,z,w)
+#define THTensor_(NAME)   TH_CONCAT_4(TH,Real,Tensor_,NAME)
+
+typedef struct THAllocator {
+  void* (*malloc)(void*, long);
+  void* (*realloc)(void*, void*, long);
+  void (*free)(void*, void*);
+} THAllocator;
+
+typedef struct THStorage
+{
+    real *data;
+    long size;
+    int refcount;
+    char flag;
+    THAllocator *allocator;
+    void *allocatorContext;
+    struct THStorage *view;
+} THStorage;
+
+typedef struct THTensor
+{
+    long *size;
+    long *stride;
+    int nDimension;
+
+    THStorage *storage;
+    long storageOffset;
+    int refcount;
+
+    char flag;
+
+} THTensor;
+
+int THTensor_(isSetTo)(const THTensor *self, const THTensor* src)
+{
+  if (self->storage == src->storage &&
+      self->storageOffset == src->storageOffset &&
+      self->nDimension == src->nDimension)
+  {
+    int d = 0;
+    while (__mark(42) & (d < self->nDimension)) {
+      if (self->size[d] != src->size[d] || self->stride[d] != src->stride[d])
+        return 0;
+      ++d;
+    }
+    return 1;
+  }
+  return 0;
+}
