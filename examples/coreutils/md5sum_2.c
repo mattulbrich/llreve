52d13d56b133bf82119af9c9b71a06c8029a7057
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-12-06 21:14:08 +0000

    Add md5sum example
    git-svn-id: https://svnserver.informatik.kit.edu/i57/svn/Reve@1132 c15e727d-c47f-405b-9667-70cf6562d04f

---
+++ examples/coreutils/md5sum_2.c
@@ -0,0 +1,43 @@
+typedef int bool;
+typedef int size_t;
+
+extern int __mark(int);
+#define false (0)
+#define true  (1)
+#define ISWHITE(c) ((c) == ' ' || (c) == '\t')
+
+bool
+bsd_split_3 (char *s, size_t s_len, unsigned char **hex_digest, char **file_name)
+{
+  size_t i;
+
+  if (s_len == 0)
+    return false;
+
+  *file_name = s;
+
+  /* Find end of filename. The BSD 'md5' and 'sha1' commands do not escape
+     filenames, so search backwards for the last ')'. */
+  i = s_len - 1;
+  while (__mark(1) & (i && s[i] != ')'))
+    i--;
+
+  if (s[i] != ')')
+    return false;
+
+  s[i++] = '\0';
+
+  while (__mark(2) & (ISWHITE (s[i])))
+    i++;
+
+  if (s[i] != '=')
+    return false;
+
+  i++;
+
+  while (__mark(3) & (ISWHITE (s[i])))
+    i++;
+
+  *hex_digest = (unsigned char *) &s[i];
+  return true;
+}
