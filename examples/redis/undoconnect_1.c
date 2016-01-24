d4a0b27d4cf3c1e5070772fb1742952717ae495c
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-01-24 21:05:07 +0000

    Remove dependency on external handlers
    git-svn-id: https://svnserver.informatik.kit.edu/i57/svn/Reve@1196 c15e727d-c47f-405b-9667-70cf6562d04f

--- examples/redis/undoconnect_1.c
+++ examples/redis/undoconnect_1.c
@@ -1,4 +1,20 @@
-#include "server.h"
+#define REPL_STATE_CONNECTING 2 /* Connecting to master */
+#define AE_READABLE 1
+#define AE_WRITABLE 2
+#define REPL_STATE_RECEIVE_PONG 3 /* Wait for PING reply */
+#define REPL_STATE_RECEIVE_PSYNC 11 /* Wait for PSYNC reply */
+#define REPL_STATE_CONNECT 1 /* Must connect to master */
+#define serverAssert(_e) ((_e)?(void)0 : (_serverAssert(#_e,__FILE__,__LINE__),_exit(1)))
+extern void _serverAssert(char *estr, char *file, int line);
+extern void aeDeleteFileEvent(int *eventLoop, int fd, int mask);
+extern int close(int fd);
+extern void _exit(int);
+extern struct redisServer server;
+struct redisServer {
+    int *el;
+    int repl_state;          /* Replication status if the instance is a slave */
+    int repl_transfer_s;     /* Slave -> Master SYNC socket */
+};
 
 int slaveIsInHandshakeState(void) {
     return (server.repl_state >= REPL_STATE_RECEIVE_PONG &&
--- examples/redis/undoconnect_2.c
+++ examples/redis/undoconnect_2.c
@@ -7,7 +7,23 @@
           ((server-state (select HEAP$2 (+ server$2 (* 682 0) 160))))
         (or (= 2 server-state) (= 3 server-state) (> server-state 11) (< server-state 2))))
 @*/
-#include "server.h"
+#define REPL_STATE_CONNECTING 2 /* Connecting to master */
+#define AE_READABLE 1
+#define AE_WRITABLE 2
+#define REPL_STATE_RECEIVE_PONG 3 /* Wait for PING reply */
+#define REPL_STATE_RECEIVE_PSYNC 11 /* Wait for PSYNC reply */
+#define REPL_STATE_CONNECT 1 /* Must connect to master */
+#define serverAssert(_e) ((_e)?(void)0 : (_serverAssert(#_e,__FILE__,__LINE__),_exit(1)))
+extern void _serverAssert(char *estr, char *file, int line);
+extern void aeDeleteFileEvent(int *eventLoop, int fd, int mask);
+extern int close(int fd);
+extern void _exit(int);
+extern struct redisServer server;
+struct redisServer {
+    int *el;
+    int repl_state;          /* Replication status if the instance is a slave */
+    int repl_transfer_s;     /* Slave -> Master SYNC socket */
+};
 
 int slaveIsInHandshakeState(void) {
     return (server.repl_state >= REPL_STATE_RECEIVE_PONG &&
