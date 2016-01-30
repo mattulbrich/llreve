4e2f1201a8a079c62655b52a473901b0c8e05234
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-01-30 12:04:25 +0000

    Add memccpy example
    git-svn-id: https://svnserver.informatik.kit.edu/i57/svn/Reve@1238 c15e727d-c47f-405b-9667-70cf6562d04f

---
+++ examples/libc/memccpy_1_1.c
@@ -0,0 +1,19 @@
+/* dietlibc */
+#include <stddef.h>
+extern int __mark(int);
+void *memccpy(void *dst, const void *src, int c, size_t count)
+{
+  char *a = dst;
+  const char *b = src;
+  while (count--)
+  {
+    *a++ = *b;
+    if (*b==c)
+    {
+      return (void *)a;
+    }
+    b++;
+    __mark(0);
+  }
+  return 0;
+}
---
+++ examples/libc/memccpy_1_2.c
@@ -0,0 +1,17 @@
+/* openbsd */
+#include <stddef.h>
+extern int __mark(int);
+void *
+memccpy(void *t, const void *f, int c, size_t n)
+{
+	if (n) {
+		unsigned char *tp = t;
+		const unsigned char *fp = f;
+		unsigned char uc = c;
+		do {
+			if ((*tp++ = *fp++) == uc)
+				return (tp);
+		} while (__mark(0) & (--n != 0));
+	}
+	return (0);
+}
