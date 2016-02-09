c90f46c654435ea2d3c8f5790a6a2f44f9683fd3
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-02-09 09:30:32 +0100

    Restore original swab implementation

--- reve/examples/libc/swab_1.c
+++ reve/examples/libc/swab_1.c
@@ -5,7 +5,6 @@ void swab(const void *src, void *dest, ssize_t nbytes) {
     ssize_t i;
     const char *s = src;
     char *d = dest;
-    // CHANGED
     nbytes = (nbytes / 2) * 2;
     for (i = 0; __mark(0) & (i < nbytes); i += 2) {
         d[i] = s[i + 1];
--- reve/examples/libc/swab_2.c
+++ reve/examples/libc/swab_2.c
@@ -1,4 +1,15 @@
 /* openbsd */
+/*@ rel_in
+(and
+      (= src$1_0 from$2_0)
+      (= dest$1_0 to$2_0)
+      (= nbytes$1_0 len$2_0)
+      (forall
+         ((i Int))
+         (= (select HEAP$1 i) (select HEAP$2 i)))
+; no overlap
+      (<= (+ src$1_0 nbytes$1_0) dest$1_0))
+@*/
 #include <unistd.h>
 extern int __mark(int);
 void swab(const void *from, void *to, ssize_t len) {
@@ -7,15 +18,9 @@ void swab(const void *from, void *to, ssize_t len) {
     unsigned char t0, t1;
 
     while (__mark(0) & (len > 1)) {
-        // CHANGED: openbsd actually reads both values first, that way
-        // it works correctly when from and to are equal, sadly posix
-        // says that that the behavior is undefined if the two regions
-        // overlap, so we can’t clam to have found a bug
-        /* t0 = src[0]; */
+        t0 = src[0];
         t1 = src[1];
         dst[0] = t1;
-        // CHANGED
-        t0 = src[0];
         dst[1] = t0;
         src += 2;
         dst += 2;
