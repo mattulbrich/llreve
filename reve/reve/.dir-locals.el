f435f11ba9bd666e4644a4770c834943628adfce
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-02-18 09:29:22 +0100

    Fix .dir-locals when opening a directory

--- reve/reve/.dir-locals.el
+++ reve/reve/.dir-locals.el
@@ -1,3 +1,3 @@
 ((nil . ((projectile-project-compilation-cmd . "cd reve/reve/build && make -j8")
-         (eval . (if (string-equal (file-name-extension (buffer-file-name)) "h")
+         (eval . (if (and (buffer-file-name) (string-equal (file-name-extension (buffer-file-name)) "h"))
                      (c++-mode))))))
