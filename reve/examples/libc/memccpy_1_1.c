322c74b1d00a8ba8dcce309b3b6899e513bff464
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-03-06 16:12:02 +0100

    Format memccpy example

--- reve/examples/libc/memccpy_1_1.c
+++ reve/examples/libc/memccpy_1_1.c
@@ -1,19 +1,16 @@
 /* dietlibc */
 #include <stddef.h>
 extern int __mark(int);
-void *memccpy(void *dst, const void *src, int c, size_t count)
-{
-  char *a = dst;
-  const char *b = src;
-  while (count--)
-  {
-    *a++ = *b;
-    if (*b==c)
-    {
-      return (void *)a;
+void *memccpy(void *dst, const void *src, int c, size_t count) {
+    char *a = dst;
+    const char *b = src;
+    while (count--) {
+        *a++ = *b;
+        if (*b == c) {
+            return (void *)a;
+        }
+        b++;
+        __mark(0);
     }
-    b++;
-    __mark(0);
-  }
-  return 0;
+    return 0;
 }
