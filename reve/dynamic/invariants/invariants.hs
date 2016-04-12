5c263598e34baca7a66392ef199126943351d30a
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-04-12 11:27:22 +0200

    Update script to record invariants

--- reve/dynamic/invariants/invariants.hs
+++ reve/dynamic/invariants/invariants.hs
@@ -2,12 +2,11 @@
 -- stack --resolver nightly-2016-04-10 --install-ghc runghc --package turtle --package optparse-generic
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
-import qualified Data.Set as S
+import           Control.Monad
 import qualified Data.Text as T
 import           Data.Maybe
 import           Prelude hiding (FilePath)
 import           Turtle
-import qualified Control.Foldl as F
 import           Options.Generic
 
 data Opts =
@@ -20,18 +19,20 @@ main :: IO ()
 main =
   do opts <- getRecord "invariants"
      output (out opts) $
-       do file <- find (suffix ".smt2") (dir opts)
-          guard $ not (any (`T.isSuffixOf`format fp file) ignoredList)
+       do file <-
+            find (suffix ".smt2")
+                 (dir opts)
+          guard $ not (any (`T.isSuffixOf` format fp file) ignoredList)
           echo (format fp file)
           invariant <- getInvariant (file)
-          pure $
-            format (fp % ": " %s)
-                   file
-                   (fromMaybe "ERROR" invariant)
+          case invariant of
+            Nothing -> pure $ format (fp % ": ERROR") file
+            Just invariants ->
+              pure (format (fp % ":") file) <|> (select invariants) <|> pure ""
 
-getInvariant :: MonadIO io => FilePath -> io (Maybe Text)
+getInvariant :: MonadIO io => FilePath -> io (Maybe [T.Text])
 getInvariant file =
-  flip fold F.last $ inproc "eld" (["-ssol",file'] <> abstract) ""
+  flip fold invariantFold $ inproc "eld" (["-ssol",file'] <> abstract) ""
   where abstract =
           guard (any (`T.isSuffixOf` file') abstractList) >>
           pure "-abstract:off"
@@ -39,7 +40,19 @@ getInvariant file =
 
 
 abstractList :: [T.Text]
-abstractList = ["rec/mccarthy91.smt2","rec/ackermann.smt2","heap/swaparray.smt2","heap/selsort.smt2","libc/strcspn_3.smt2"]
+abstractList =
+  ["rec/mccarthy91.smt2"
+  ,"rec/ackermann.smt2"
+  ,"heap/swaparray.smt2"
+  ,"heap/selsort.smt2"
+  ,"libc/strcspn_3.smt2"]
 
 ignoredList :: [T.Text]
 ignoredList = ["libc/strncasecmp_1.smt2"]
+
+invariantFold :: Fold T.Text (Maybe [T.Text])
+invariantFold = Fold step Nothing (fmap ($[]))
+  where step :: Maybe ([T.Text] -> [T.Text]) -> T.Text -> Maybe ([T.Text] -> [T.Text])
+        step Nothing "sat" = Just id
+        step Nothing _ = Nothing
+        step (Just f) x = Just (f . (x:))
