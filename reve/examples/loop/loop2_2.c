5251996c4bfebf8115f0bd4d7178d56c91d1e523
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-04-18 18:03:26 +0200

    Move marks in loop2 to ease understanding

--- reve/examples/loop/loop2_2.c
+++ reve/examples/loop/loop2_2.c
@@ -1,11 +1,10 @@
-extern void __mark(int);
+extern int __mark(int);
 
 int f(int n) {
     int i = 0;
     int j = 0;
 
-    while (i < n) {
-        __mark(42);
+    while (__mark(42) & (i < n)) {
         j = j + 2;
         i++;
     }
