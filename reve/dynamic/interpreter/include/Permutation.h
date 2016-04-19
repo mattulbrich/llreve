155ebc8d0b8bbab182f72622cc8a981287fab911
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-04-19 08:09:14 +0200

    Split up permutation and subset generation

--- reve/dynamic/interpreter/include/Permutation.h
+++ reve/dynamic/interpreter/include/Permutation.h
@@ -1,5 +1,29 @@
 #pragma once
 
+
+template <typename T>
+std::list<std::vector<T>> kSubset(std::vector<T> input, size_t k) {
+    std::list<std::vector<T>> output;
+    assert(input.size() < 64);
+    uint64_t bitVec = (1 << k) - 1;
+    do {
+        std::vector<T> subset(k);
+        size_t j = 0;
+        for (size_t i = 0; i < input.size(); ++i) {
+            if (bitVec & (1 << i)) {
+                subset.at(j) = input.at(i);
+                ++j;
+            }
+        }
+        output.push_back(subset);
+        // Dark magic from
+        // https://graphics.stanford.edu/~seander/bithacks.html#NextBitPermutation
+        uint64_t t = (bitVec | (bitVec - 1)) + 1;
+        bitVec = t | ((((t & -t) / (bitVec & -bitVec)) >> 1) - 1);
+    } while (!(bitVec & (1 << input.size())));
+    return output;
+}
+
 // Somebody should really write a test for this :P
 template <typename T>
 std::list<std::vector<T>> kPermutations(std::vector<T> input, size_t k) {
@@ -8,32 +32,14 @@ std::list<std::vector<T>> kPermutations(std::vector<T> input, size_t k) {
         return {};
     }
     std::list<std::vector<T>> output;
-    assert(input.size() < 64);
-    uint64_t bitVec = (1 << k) - 1;
-    do {
-        // take the first k elements
-        std::vector<T> firstKElements(k);
-        size_t j = 0;
-        for (size_t i = 0; i < input.size(); ++i) {
-            if (bitVec & (1 << i)) {
-                firstKElements.at(j) = input.at(i);
-                ++j;
-            }
-        }
+    std::list<std::vector<T>> subsets = kSubset(input, k);
+    for (auto vec : subsets) {
         // Sort for next_permutation
-        std::sort(firstKElements.begin(), firstKElements.end());
-        assert(j == k);
+        std::sort(vec.begin(), vec.end());
         do {
-            output.push_back(firstKElements);
-        } while (std::next_permutation(firstKElements.begin(),
-                                       firstKElements.end()));
-        // Dark magic from
-        // https://graphics.stanford.edu/~seander/bithacks.html#NextBitPermutation
-        uint64_t t = (bitVec | (bitVec - 1)) + 1;
-        bitVec = t | ((((t & -t) / (bitVec & -bitVec)) >> 1) - 1);
-        if (bitVec & (1 << input.size())) {
-            break;
-        }
-    } while (true);
+            output.push_back(vec);
+        } while (std::next_permutation(vec.begin(),
+                                       vec.end()));
+    }
     return output;
 }
