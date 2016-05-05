2f7391c5ba1d454781423e49632d0b9eaf5799bf
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-05-05 08:43:54 +0200

    Fix precedence warnings in barthe2-big2

--- reve/examples/loop/barthe2-big2_2.c
+++ reve/examples/loop/barthe2-big2_2.c
@@ -3,19 +3,19 @@ int f(int n) {
     int i = 1;
     int x = 1;
 
-    while (__mark(1) & i <= n) {
+    while (__mark(1) & (i <= n)) {
         x = x * 1;
         i++;
     }
 
     i = 1;
-    while (__mark(2) & i <= n) {
+    while (__mark(2) & (i <= n)) {
         x = x + i;
         i++;
     }
 
     i = 1;
-    while (__mark(3) & i <= n) {
+    while (__mark(3) & (i <= n)) {
         x = x * 2;
         i++;
     }
