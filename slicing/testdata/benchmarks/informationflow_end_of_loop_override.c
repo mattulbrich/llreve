14a188f673942d23d90f5f9f88f9f973641a68fd
Stephan Gocht <stephan@gobro.de>
2016-05-28 22:47:10 +0200

    Improved example.

--- slicing/testdata/benchmarks/informationflow_end_of_loop_override.c
+++ slicing/testdata/benchmarks/informationflow_end_of_loop_override.c
@@ -1,9 +1,9 @@
-int foo(int heigh, int low) {
-	for ( int i = 0; i < 6 ; i ++) {
-		if ( i < 4)
-			low = heigh ;
+int foo(int heigh, int low, int N) {
+	for ( int i = 0; i < N ; i ++) {
+		if ( i < N - 1)
+			low = heigh + 0 ;
 		else
-			low = 3;
+			low = 3 + 0;
 	}
 	return low ;
 }
