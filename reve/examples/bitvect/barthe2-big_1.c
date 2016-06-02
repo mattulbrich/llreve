4ae3b9fc7d85a89f7edabe7a0ef2c9570a028808
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-06-02 21:02:36 +0200

    Add barthe2-big example

---
+++ reve/examples/bitvect/barthe2-big_1.c
@@ -0,0 +1,20 @@
+/*@ opt -nest @*/
+/*@ pre (bvsge n$1_0__1 (_ bv0 32)) (bvsle n$1_0__1 (_ bv13 32)) @*/
+extern int __mark(int);
+int f(int n) {
+    int i = 1;
+    int x = 1;
+
+    while (__mark(1) & i <= n) {
+        x = x * 5;
+        i++;
+    }
+
+    i = 0;
+
+    while (__mark(2) & i <= n) {
+        x = x + i;
+        i++;
+    }
+    return x;
+}
