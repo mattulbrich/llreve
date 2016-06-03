b34955203e01f41d4024e745bc1ba8e861eebaaa
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-06-03 16:07:59 +0200

    Add andmod bitvect example

---
+++ reve/examples/bitvect/andmod_2.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+extern int __mark(int);
+uint32_t f(uint32_t x) {
+    uint32_t sum = 0;
+    for (uint32_t i = 0; __mark(0) & (i < x); ++i) {
+        if (i % 2 == 1) {
+            sum += i;
+        }
+    }
+    return sum;
+}
