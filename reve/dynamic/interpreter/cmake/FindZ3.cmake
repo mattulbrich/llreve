fecf0dbac632ddc32f226eb709f4b83e7b979397
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-08-11 14:06:32 +0200

    Fix license headers in cmake files

--- reve/dynamic/interpreter/cmake/FindZ3.cmake
+++ reve/dynamic/interpreter/cmake/FindZ3.cmake
@@ -1,12 +1,11 @@
-/*
- * This file is part of
- *    llreve - Automatic regression verification for LLVM programs
- *
- * Copyright (C) 2016 Karlsruhe Institute of Technology
- *
- * The system is published under a BSD license.
- * See LICENSE (distributed with this file) for details.
- */
+#
+# This file is part of
+#    llreve - Automatic regression verification for LLVM programs
+#
+# Copyright (C) 2016 Karlsruhe Institute of Technology
+#
+# The system is published under a BSD license.
+# See LICENSE (distributed with this file) for details.
 
 # Try to find the GMP librairies
 # GMP_FOUND - system has GMP lib
