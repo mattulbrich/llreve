bcf8c7671377fd60568436a97b60bffb5439d809
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-10-08 11:56:09 +0200

    Add gcd examples

---
+++ reve/examples/stack/gcd.c.rec.c
@@ -0,0 +1,20 @@
+void swap(short *a, short *b) {
+    short tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+
+unsigned char Loop_gcd_while1(short *rvp_a, short *rvp_b, short *rvp_rvretval) {
+    if (!(*rvp_a != 0))
+        return 0;
+    *rvp_b = *rvp_b % *rvp_a;
+    swap(&*rvp_a, &*rvp_b);
+    return Loop_gcd_while1(rvp_a, rvp_b, rvp_rvretval);
+}
+
+short gcd(short a, short b) {
+    short rvretval = 0;
+    Loop_gcd_while1(&a, &b, &rvretval);
+    return b;
+}
