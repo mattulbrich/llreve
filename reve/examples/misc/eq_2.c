d6c634d9a373ce88d78f05a4223a0cdfb3784e86
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-10-14 09:48:38 +0200

    Integrate -assume-equivalent/-couple-functions testcase

--- reve/examples/misc/eq_2.c
+++ reve/examples/misc/eq_2.c
@@ -1,5 +1,15 @@
+/*@ opt -disable-auto-coupling @*/
+/*@ opt -disable-auto-abstraction @*/
+/*@ opt -couple-functions=g,a @*/
+/*@ opt -couple-functions=h,b @*/
+/*@ opt -couple-functions=f,f @*/
+/*@ opt -fun f @*/
+/*@ opt -assume-equivalent=h,b @*/
+/*@ opt -assume-equivalent=g,a @*/
+
+// g and a are _not_ equivalent so this tests if we are correctly ignoring the source code if --assume-equivalent=g,a is passed
 int a(int x) {
-    return x;
+    return x+1;
 }
 // extern int a(int x);
 extern int b(int x);
