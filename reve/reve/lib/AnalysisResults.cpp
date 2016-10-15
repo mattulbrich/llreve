fef084fcca75576e6dad06f9b75b27bb7aa358f4
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-10-15 14:21:01 +0200

    remove debug output

--- reve/reve/lib/AnalysisResults.cpp
+++ reve/reve/lib/AnalysisResults.cpp
@@ -5,8 +5,6 @@ using std::string;
 
 MonoPair<PathMap> getPathMaps(MonoPair<llvm::Function *> functions,
                               const AnalysisResultsMap &analysisResults) {
-    logWarning("functionNames: " + functions.first->getName().str() + ", " +
-               functions.second->getName().str() + "\n");
     return makeMonoPair(analysisResults.at(functions.first).paths,
                         analysisResults.at(functions.second).paths);
 }
