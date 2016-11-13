7f7643dcee8eed927432d9215c9a40a7b4220cae
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-11-13 11:52:26 +0100

    Simplify populateEquationsMap

--- reve/dynamic/llreve-dynamic/lib/llreve/dynamic/PolynomialEquation.cpp
+++ reve/dynamic/llreve-dynamic/lib/llreve/dynamic/PolynomialEquation.cpp
@@ -10,30 +10,61 @@ using smt::SortedVar;
 
 namespace llreve {
 namespace dynamic {
+
+static mpz_class evalTerm(const std::vector<std::string> &term,
+                          const VarMap<string> &variables) {
+    mpz_class termVal = 1;
+    for (const auto &var : term) {
+        termVal *= unsafeIntVal(variables.at(var)).asUnbounded();
+    }
+    return termVal;
+}
+
+static vector<mpq_class>
+createEquation(const vector<SortedVar> &primitiveVariables,
+               const VarMap<string> &variables, unsigned maxDegree) {
+    vector<mpq_class> equation;
+    for (size_t i = 1; i <= maxDegree; ++i) {
+        auto polynomialTerms = polynomialTermsOfDegree(primitiveVariables, i);
+        for (const auto &term : polynomialTerms) {
+            equation.push_back(evalTerm(term, variables));
+        }
+    }
+    // this represents the constant
+    equation.push_back(1);
+    return equation;
+}
+
+static void insertInStringMap(const FastVarMap &variables,
+                              VarMap<string> &stringVariables) {
+    for (auto varIt : variables) {
+        stringVariables.insert({varIt.first->getName(), varIt.second});
+    }
+}
+
+static VarMap<string> getStringVarMap(const FastVarMap &variables) {
+    VarMap<string> stringVariables;
+    insertInStringMap(variables, stringVariables);
+    return stringVariables;
+}
+
+static VarMap<string> getStringVarMap(const FastVarMap &variables1,
+                                      const FastVarMap &variables2) {
+    VarMap<string> stringVariables;
+    insertInStringMap(variables1, stringVariables);
+    insertInStringMap(variables2, stringVariables);
+    return stringVariables;
+}
+
 void populateEquationsMap(
     IterativeInvariantMap<PolynomialEquations> &polynomialEquations,
     vector<smt::SortedVar> primitiveVariables,
     MatchInfo<const llvm::Value *> match, ExitIndex exitIndex, size_t degree) {
-    VarMap<string> variables;
-    for (auto varIt : match.steps.first->state.variables) {
-        variables.insert(std::make_pair(varIt.first->getName(), varIt.second));
-    }
-    for (auto varIt : match.steps.second->state.variables) {
-        variables.insert(std::make_pair(varIt.first->getName(), varIt.second));
-    }
-    vector<mpq_class> equation;
-    for (size_t i = 1; i <= degree; ++i) {
-        auto polynomialTerms = polynomialTermsOfDegree(primitiveVariables, i);
-        for (auto term : polynomialTerms) {
-            mpz_class termVal = 1;
-            for (auto var : term) {
-                termVal *= unsafeIntVal(variables.at(var)).asUnbounded();
-            }
-            equation.push_back(termVal);
-        }
-    }
-    // Add a constant at the end of each vector
-    equation.push_back(1);
+    VarMap<string> variables =
+        getStringVarMap(match.steps.first->state.variables,
+                        match.steps.second->state.variables);
+    vector<mpq_class> equation =
+        createEquation(primitiveVariables, variables, degree);
     if (polynomialEquations[match.mark].count(exitIndex) == 0) {
         polynomialEquations.at(match.mark)
             .insert(make_pair(exitIndex,
@@ -59,42 +90,19 @@ void populateEquationsMap(
     vector<SortedVar> primitiveVariables,
     CoupledCallInfo<const llvm::Value *> match, size_t degree) {
     auto &polynomialEquations = equationsMap[match.functions];
-    VarMap<string> variables;
-    for (auto varIt : match.steps.first->state.variables) {
-        variables.insert(std::make_pair(varIt.first->getName(), varIt.second));
-    }
-    for (auto varIt : match.steps.second->state.variables) {
-        variables.insert(std::make_pair(varIt.first->getName(), varIt.second));
-    }
+    VarMap<string> variables =
+        getStringVarMap(match.steps.first->state.variables,
+                        match.steps.second->state.variables);
     variables.insert({resultName(Program::First), match.returnValues.first});
     variables.insert({resultName(Program::Second), match.returnValues.second});
     vector<smt::SortedVar> preVariables = primitiveVariables;
     vector<smt::SortedVar> postVariables = preVariables;
     postVariables.emplace_back(resultName(Program::First), int64Type());
     postVariables.emplace_back(resultName(Program::Second), int64Type());
-    vector<mpq_class> preEquation;
-    vector<mpq_class> postEquation;
-    for (size_t i = 1; i <= degree; ++i) {
-        auto prePolynomialTerms = polynomialTermsOfDegree(preVariables, i);
-        auto postPolynomialTerms = polynomialTermsOfDegree(postVariables, i);
-        for (auto term : prePolynomialTerms) {
-            mpz_class termVal = 1;
-            for (auto var : term) {
-                termVal *= unsafeIntVal(variables.at(var)).asUnbounded();
-            }
-            preEquation.push_back(termVal);
-        }
-        for (auto term : postPolynomialTerms) {
-            mpz_class termVal = 1;
-            for (auto var : term) {
-                termVal *= unsafeIntVal(variables.at(var)).asUnbounded();
-            }
-            postEquation.push_back(termVal);
-        }
-    }
-    // Add a constant at the end of each vector
-    preEquation.push_back(1);
-    postEquation.push_back(1);
+    vector<mpq_class> preEquation =
+        createEquation(preVariables, variables, degree);
+    vector<mpq_class> postEquation =
+        createEquation(postVariables, variables, degree);
     if (polynomialEquations.count(match.mark) == 0) {
         polynomialEquations.insert(
             {match.mark, {{{}, {}}, {{}, {}}, {{}, {}}}});
@@ -121,37 +129,15 @@ void populateEquationsMap(FunctionInvariantMap<Matrix<mpq_class>> &equationsMap,
                           UncoupledCallInfo<const llvm::Value *> match,
                           size_t degree) {
     auto &polynomialEquations = equationsMap[match.function];
-    VarMap<string> variables;
-    for (auto varIt : match.step->state.variables) {
-        variables.insert(std::make_pair(varIt.first->getName(), varIt.second));
-    }
+    VarMap<string> variables = getStringVarMap(match.step->state.variables);
     variables.insert({resultName(match.prog), match.returnValue});
     vector<smt::SortedVar> preVariables = primitiveVariables;
     vector<smt::SortedVar> postVariables = preVariables;
     postVariables.emplace_back(resultName(match.prog), int64Type());
-    vector<mpq_class> preEquation;
-    vector<mpq_class> postEquation;
-    for (size_t i = 1; i <= degree; ++i) {
-        auto prePolynomialTerms = polynomialTermsOfDegree(preVariables, i);
-        auto postPolynomialTerms = polynomialTermsOfDegree(postVariables, i);
-        for (auto term : prePolynomialTerms) {
-            mpz_class termVal = 1;
-            for (auto var : term) {
-                termVal *= unsafeIntVal(variables.at(var)).asUnbounded();
-            }
-            preEquation.push_back(termVal);
-        }
-        for (auto term : postPolynomialTerms) {
-            mpz_class termVal = 1;
-            for (auto var : term) {
-                termVal *= unsafeIntVal(variables.at(var)).asUnbounded();
-            }
-            postEquation.push_back(termVal);
-        }
-    }
-    // Add a constant at the end of each vector
-    preEquation.push_back(1);
-    postEquation.push_back(1);
+    vector<mpq_class> preEquation =
+        createEquation(preVariables, variables, degree);
+    vector<mpq_class> postEquation =
+        createEquation(postVariables, variables, degree);
     auto polynomialEquationsIt = polynomialEquations.find(match.mark);
     if (polynomialEquationsIt == polynomialEquations.end()) {
         polynomialEquations.insert(
