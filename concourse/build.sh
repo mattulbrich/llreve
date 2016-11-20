e6459d91929a1213deea3baa39a11d5aff4ff647
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-11-20 20:57:52 +0100

    Update pipeline

---
+++ concourse/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}/../reve/reve"
+mkdir build
+cd build
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=-static
+ninja
+cp reve "${DIR}/../../static-binaries/"
