723e16667c56244b7595c5102c2ed2327a7da105
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-11-21 20:30:41 +0100

    Avoid building llreve twice

--- concourse/build.sh
+++ concourse/build.sh
@@ -2,15 +2,10 @@
 set -o nounset
 set -o errexit
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${DIR}/../reve/reve"
-mkdir build
-cd build
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=-static
-ninja
-cp reve "${DIR}/../../static-binaries/llreve"
 cd "${DIR}/../reve/dynamic/llreve-dynamic/"
 mkdir build
 cd build
 cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=-static
 ninja
+cp reve/reve "${DIR}/../../static-binaries/llreve"
 cp llreve-dynamic "${DIR}/../../static-binaries/llreve-dynamic"
