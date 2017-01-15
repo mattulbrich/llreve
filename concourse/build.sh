c02ac204d9b2ba1908124c95bee03645e6015d08
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-01-15 18:56:59 +0100

    Fix concourse build script and docker build script

--- concourse/build.sh
+++ concourse/build.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}/../reve/"
 mkdir build
 cd build
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=-static
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DLLREVE_STATIC=ON
 ninja
 cp reve/llreve "${DIR}/../../static-binaries/llreve"
 cp dynamic/llreve-dynamic/llreve-dynamic "${DIR}/../../static-binaries/llreve-dynamic"
--- reve/docker/build_static.sh
+++ reve/docker/build_static.sh
@@ -4,15 +4,12 @@ set -o nounset
 set -o errexit
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LLREVE_DIR="${DIR}/../../"
-BUILD_COMMAND='rm -rf /llreve/reve/reve/build /llreve/reve/dynamic/llreve-dynamic/build && \
-               mkdir /llreve/reve/reve/build /llreve/reve/dynamic/llreve-dynamic/build && \
+BUILD_COMMAND='rm -rf /llreve/reve/build && \
+               mkdir /llreve/reve/build && \
                echo "Building llreve" && \
-               cd /llreve/reve/reve/build && \
-               cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=-static && \
-               ninja && \
-               echo "Building llreve-dynamic" && \
-               cd /llreve/reve/dynamic/llreve-dynamic/build && \
-               cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=-static && \
+               cd /llreve/reve/build && \
+               cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release \
+                                -DLLREVE_STATIC=ON \
                ninja'
 CONTAINER="$(docker create cocreature/llreve:llvm-3.9_z3-4.5 sh -c "${BUILD_COMMAND}")"
 function finish {
@@ -22,10 +19,10 @@ trap finish EXIT
 echo "Created container with id ${CONTAINER}"
 echo "Copying llreve repository in container"
 docker cp "${LLREVE_DIR}" $CONTAINER:/llreve
-echo "Starting build of llreve"
+echo "Starting build"
 docker start $CONTAINER
 docker wait $CONTAINER
 echo "Copying llreve into the current directory"
-docker cp $CONTAINER:/llreve/reve/reve/build/reve llreve
+docker cp $CONTAINER:/llreve/reve/build/reve/llreve llreve
 echo "Copying llreve-dynamic into the current directory"
-docker cp $CONTAINER:/llreve/reve/dynamic/llreve-dynamic/build/llreve-dynamic llreve-dynamic
+docker cp $CONTAINER:/llreve/reve/build/dynamic/llreve-dynamic/llreve-dynamic llreve-dynamic
