2f643351f717ee77f4970f2659b979091fd495da
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-02-06 20:36:18 +0100

    Delete obsolete dir-locals

--- reve/reve/.dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((projectile-project-compilation-cmd . "cd reve/reve/build && make -j8")
-         (eval . (if (and (buffer-file-name) (string-equal (file-name-extension (buffer-file-name)) "h"))
-                     (c++-mode))))))
